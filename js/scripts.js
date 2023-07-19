b23bee5e67920ac8ebb726aff1b3f4904f5a8a03
ShadowOfBeing <82723137+ShadowOfBeing@users.noreply.github.com>
2023-07-19 19:08:29 +0300

    upd 19:08 19.07.2023

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,22 @@ function getLayout(category) {
 function getStep(category, step, stepValue) {
     step = parseInt(step)
     var tmpStep = step + stepValue
+    var pages = ''
     if (tmpStep in messages[category]) {
         step = tmpStep
         sessionStorage.setItem("step", step)
         //document.getElementsByClassName('chat-wrapper')[0].insertAdjacentHTML('afterbegin', messages[category][step])
         document.getElementsByClassName('text-wrapper')[0].innerHTML = messages[category][step]
+        for (let i in messages[category]) {
+            let active = i == step ? 'active' : ''
+            pages += `<div class="page ${active}" onclick="getStep('${category}', ${step}, ${i - step})">${i}</div>`
+        }
+        document.getElementById('pages').innerHTML = pages
 //        document.getElementsByClassName('chat-wrapper')[0].innerHTML =
 //        `
 //            ${messages[category][step]}
-//            <div class="prev-btn" onclick="getStep(category, sessionStorage.getItem('step'), -1)">вернуться на шаг назад</div>
-//            <div class="next-btn" onclick="getStep(category, sessionStorage.getItem('step'), 1)">я всё понял, идём дальше</div>
+//            <div id="prev-btn" onclick="getStep(category, sessionStorage.getItem('step'), -1)">вернуться на шаг назад</div>
+//            <div id="next-btn" onclick="getStep(category, sessionStorage.getItem('step'), 1)">я всё понял, идём дальше</div>
 //        `
     }
 }
